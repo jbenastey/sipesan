bcbc55a184492e99d04c50c1626c52ac2db41e65
jbenastey <jbenastey@gmail.com>
2019-07-18 15:19:42 +0700

    add backend templates

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'HomeController';
 
+$route['admin'] = 'DashboardController';
+
 //$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/backend/templates/footer.php
@@ -0,0 +1,44 @@
+</div>
+</div>
+<!-- partial:partials/_footer.html -->
+<footer class="footer">
+	<div class="container-fluid clearfix">
+		<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2017 <a href="#">UrbanUI</a>. All rights reserved.</span>
+		<span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+	</div>
+</footer>
+<!-- partial -->
+</div>
+<!-- row-offcanvas ends -->
+</div>
+<!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+
+<!-- plugins:js -->
+<script src="<?=base_url()?>assets/backend/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<script src="<?=base_url()?>assets/backend/node_modules/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/chart.js/dist/Chart.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/raphael/raphael.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/morris.js/morris.min.js"></script>
+<script src="<?=base_url()?>assets/backend/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="<?=base_url()?>assets/backend/js/off-canvas.js"></script>
+<script src="<?=base_url()?>assets/backend/js/hoverable-collapse.js"></script>
+<script src="<?=base_url()?>assets/backend/js/misc.js"></script>
+<script src="<?=base_url()?>assets/backend/js/settings.js"></script>
+<script src="<?=base_url()?>assets/backend/js/todolist.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="<?=base_url()?>assets/backend/js/dashboard.js"></script>
+<!-- End custom js for this page-->
+</body>
+
+
+</html>
