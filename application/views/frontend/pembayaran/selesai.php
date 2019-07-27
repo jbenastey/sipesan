e46e108e84d9dfba12c60aebb89b691077a0b984
jbenastey <jbenastey@gmail.com>
2019-07-27 22:41:50 +0700

    unfix checkout

--- application/views/frontend/pembayaran/selesai.php
+++ application/views/frontend/pembayaran/selesai.php
@@ -3,9 +3,8 @@
 	<div class="payment-success-icon fa fa-check-circle-o"></div>
 	<div class="payment-success-title-area">
 		<h1 class="payment-success-title"><?= $this->session->userdata('session_username');?>, Terima Kasih Telah Memesan</h1>
-		<p class="lead">Silahkan transfer ke rekening <b><?=$bank?></b> sebesar <b>Rp. <?=nominal($pesanan['keranjang_total'])?></b> sebelum 1x24 jam.</p>
-
-			<p>Kemudian konfirmasi pembayaran DISINI.
+		<p class="lead">Silahkan transfer ke rekening <b><?=$bank?></b> sebesar <b>Rp. <?=nominal($pesanan['keranjang_total'])?></b> sebelum 1x24 jam.
+			<br>Kemudian konfirmasi pembayaran <a href="#">DISINI</a>.
 		</p>
 	</div>
 	<div class="gap gap-small"></div>
